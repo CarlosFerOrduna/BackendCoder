21129511aca3f7bfbd5d96ad71e57e57ce573fca
CarlosFerOrduna <fernandoorduna.co@gmail.com>
2023-09-20 20:07:52 -0300

    test handlebars

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ connectMongo(connectionString);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.engine('handlebars', handlebars.engine());
-app.set('views', `${__dirname}/views`);
+app.set('views', './views');
 app.set('view engine', 'handlebars');
 app.use(cookieParser());
 app.use(
